015121392014f46ddcf9592d0be58236c6713266
Sujan Shrestha <shrsujan2007@gmail.com>
2025-01-18 18:38:41 -0500

    Add Users service provider
    - Add Users endpoint services
    - Inject UserService into the controller
    - Add error handling

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,31 +8,34 @@ import {
   Body,
   Query,
 } from '@nestjs/common';
+import { User, UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @Get()
-  getUsers(@Query('role') role?: 'ADMIN' | 'ENGINEER' | 'INTERN') {
-    return [{ role }];
+  getUsers(@Query('role') role?: User['role']) {
+    return this.usersService.getUsers(role);
   }
 
   @Get(':id')
-  getUser(@Param('id') id: string) {
-    return { id };
+  getUser(@Param('id') id: User['id']) {
+    return this.usersService.getUser(id);
   }
 
   @Post()
-  create(@Body() user: any) {
-    return user;
+  create(@Body() user: User) {
+    return this.usersService.create(user);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() user: any) {
-    return { id, ...user };
+  update(@Param('id') id: User['id'], @Body() user: Omit<User, 'id'>) {
+    return this.usersService.update(id, user);
   }
 
   @Delete(':id')
-  delete(@Param('id') id: string) {
-    return { id };
+  delete(@Param('id') id: User['id']) {
+    return this.usersService.delete(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,109 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+
+export enum Role {
+  ADMIN = 'ADMIN',
+  ENGINEER = 'ENGINEER',
+  INTERN = 'INTERN',
+}
+
+export type User = { id: string; role: Role; name: string; email: string };
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = [
+    { id: '1', role: Role.ADMIN, name: 'Alice', email: 'alice@test.com' },
+    { id: '2', role: Role.ENGINEER, name: 'Bob', email: 'bob@test.com' },
+    { id: '3', role: Role.INTERN, name: 'Charlie', email: 'charlie@tes.com' },
+    { id: '4', role: Role.ADMIN, name: 'David', email: 'david@test.com' },
+    { id: '5', role: Role.ENGINEER, name: 'Eve', email: 'eve@test.com' },
+  ];
+
+  getUsers(role?: Role): User[] {
+    try {
+      if (!role) {
+        return this.users;
+      }
+
+      return this.users.filter((user) => user.role === role);
+    } catch (error) {
+      throw new HttpException(
+        error.message || 'Users could not be retrieved',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  getUser(id: string): User {
+    try {
+      const index = this.users.findIndex((user) => user.id === id);
+
+      if (index <= -1) {
+        throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+      }
+
+      return this.users[index];
+    } catch (error) {
+      throw new HttpException(
+        error.message || 'User could not be retrieved',
+        error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  create(user: Omit<User, 'id'>): User {
+    try {
+      const usersByHighestId = this.users.sort((a, b) => +b.id - +a.id);
+      const newUser = {
+        id: (+usersByHighestId[0].id + 1).toString(),
+        ...user,
+      };
+      this.users.push(newUser);
+
+      return newUser;
+    } catch (error) {
+      throw new HttpException(
+        error.message || 'User could not be created',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  update(id: string, user: Partial<Omit<User, 'id'>>): User {
+    try {
+      const index = this.users.findIndex((user) => user.id === id);
+
+      if (index <= -1) {
+        throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+      }
+
+      this.users[index] = { ...this.users[index], ...user };
+
+      return this.getUser(id);
+    } catch (error) {
+      throw new HttpException(
+        error.message || 'User could not be updated',
+        error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  delete(id: string): User {
+    try {
+      const index = this.users.findIndex((user) => user.id === id);
+
+      if (index <= -1) {
+        throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+      }
+
+      const user = this.users[index];
+      this.users.splice(index, 1);
+
+      return user;
+    } catch (error) {
+      throw new HttpException(
+        error.message || 'User could not be deleted',
+        error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
